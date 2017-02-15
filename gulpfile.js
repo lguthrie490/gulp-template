f524775fc1e7fb411ea2fb2c5bea09f4b07d9c32
Logan Guthrie <lguthrie490@gmail.com>
2017-02-14 21:07:47 -0800

    Makes gulp operate from directory

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,26 @@ var svgmin = require('gulp-svgmin');
 
 // Sync Task
 gulp.task('default', function(){
-	gulp.watch('src/css/*.scss', ['sass']);
-	gulp.watch('src/js/*.js', ['uglify']);
-	gulp.watch('src/img/*.svg', ['svgmin']);
+	gulp.watch('../src/css/*.scss', ['sass']);
+	gulp.watch('../src/js/*.js', ['uglify']);
+	gulp.watch('../src/img/*.svg', ['svgmin']);
 });
 
 // Gulp Tasks
 gulp.task('sass', function(){
-        return gulp.src('src/css/*.scss')
+        return gulp.src('../src/css/*.scss')
         .pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('../'))
 });
 
 gulp.task('uglify', function(){
-        return gulp.src('src/js/*.js')
+        return gulp.src('../src/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest('../app/js'))
 });
 
 gulp.task('svgmin', function () {
-	return gulp.src('src/img/*.svg')
+	return gulp.src('../src/img/*.svg')
 		.pipe(svgmin())
-		.pipe(gulp.dest('./img'));
+		.pipe(gulp.dest('../img'));
 });
